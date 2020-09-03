c784a59e1646e620f637b03b55f941a89e282f6b
Michael Chiu <michael.sm.chiu@gmail.com>
2020-09-03 15:54:23 -0400

    Added  initial structure of the file archiver.
    At the moment works as a one file / directory solution, need to add options to archive multiple directories into separate zip files as well as a modular method of naming zip files, such as name-date-filenum-size or date-olddirectory-filetype; options to name files such that it is easy for the user to identify what is exactly stored inside the zip files (ie common themes, filetypes, folder location, date or etc.).

---
+++ Windows file zip archiver.py
@@ -0,0 +1,27 @@
+#Windows file archiver
+import zipfile, os, shutil
+
+def main():
+    src = input("Please enter the complete file extension or folder of where/what you want to archive:\n")
+    dest = input("Enter the desired destination for the zipped file, leave empty if destination is the same locationas the source:\n")
+   
+    #Set dest to src if dest is not given
+    if dest  == '':
+        dest = src
+    
+    validpath(src, dest)
+    
+    #Check to see if src and destination are valid directories
+
+def validpath(*argv):
+    for path in argv:
+        path = os.path.abspath(path)
+        if not(os.path.exists(path)):
+            print(path, 'is not a valid directory or file.')
+            break
+            
+
+if __name__ == '__main__':
+    main()
+
+
